0750538cbb38d156cb25d4ec911ff6049963fe5a
Raphael Michel <mail@raphaelmichel.de>
2020-08-30 21:24:53 +0200

    Add settings activity

---
+++ app/src/main/java/io/rami/barcodekeyboard/SettingsActivity.kt
@@ -0,0 +1,28 @@
+package io.rami.barcodekeyboard
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_settings.*
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                10
+            )
+        }
+    }
+}
